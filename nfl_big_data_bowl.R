4ae4e49437a0c463f663246f11446a69ed4b5a9f
lanvu301 <vnlan123@gmail.com>
2025-11-03 00:04:27 -0500

    Update rmd file

--- nfl_big_data_bowl.R
+++ nfl_big_data_bowl.R
@@ -1,278 +1,62 @@
-plays <- read.csv("plays.csv")
-View(plays)
-
-dim(plays)        #  (rows, columns)
-
-head(plays,6)
-
-str(plays)  
-
-#Summary data
-# yardsGained
-summary(plays$yardsGained)
-sd(plays$yardsGained, na.rm = TRUE)
-
-# passLength
-summary(plays$passLength)
-sd(plays$passLength, na.rm = TRUE)
-
-
-# Frequency table
-table(plays$pff_passCoverage)
-
-# Proportions
-prop.table(table(plays$pff_passCoverage))
-
-
-#Check for na
-sum(is.na(plays$yardsGained))
-sum(is.na(plays$passLength))
-sum(is.na(plays$pff_passCoverage))
-
-#2 variable summary
 library(dplyr)
-
-plays %>%
-  group_by(pff_passCoverage) %>%
-  summarise(
-    Min = min(yardsGained, na.rm = TRUE),
-    Q1 = quantile(yardsGained, 0.25, na.rm = TRUE),
-    Median = median(yardsGained, na.rm = TRUE),
-    Q3 = quantile(yardsGained, 0.75, na.rm = TRUE),
-    Max = max(yardsGained, na.rm = TRUE),
-    Mean = mean(yardsGained, na.rm = TRUE),
-    SD = sd(yardsGained, na.rm = TRUE)
-  )
-
-#Visualize the numeric variable
-library(ggplot2)
-
-# Pass Length vs Yards Gained
-ggplot(df_4th, aes(x = passLength, y = yardsGained)) +
-  geom_point(alpha = 0.5) +
-  geom_smooth(method = "lm", color = "red", se = FALSE) +
-  labs(title = "Pass Length vs Yards Gained (4th Down)",
-       x = "Pass Length (Air Yards)", y = "Yards Gained")
-
-# Coverage Type vs Yards Gained
-ggplot(df_4th, aes(x = pff_passCoverage, y = yardsGained)) +
-  geom_boxplot(fill = "lightblue") +
-  labs(title = "Yards Gained by Coverage Type (4th Down)",
-       x = "PFF Coverage Type", y = "Yards Gained") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-#Catagorial variable
-table(df_4th$pff_passCoverage)
-table(df_4th$offenseFormation)
-table(df_4th$playAction)
-table(df_4th$dropbackType)
-table(df_4th$pff_manZone)
-
-
-#Distribution of yardGain in 4th down play
-library(ggplot2)
-
-ggplot(df_4th, aes(x = yardsGained)) +
-  geom_histogram(binwidth = 2, fill = "steelblue", color = "black") +
-  labs(title = "Distribution of Yards Gained on 4th Down Plays",
-       x = "Yards Gained",
-       y = "Number of Plays") +
-  theme_minimal()
-
-#YardGained and passLength
-ggplot(df_4th, aes(x = passLength, y = yardsGained)) +
-  geom_point(alpha = 0.5, color = "darkgreen") +
-  geom_smooth(method = "lm", se = FALSE, color = "red") +
-  labs(title = "Pass Length vs. Yards Gained on 4th Down",
-       x = "Pass Length (Air Yards)",
-       y = "Total Yards Gained") +
-  theme_minimal()
-
-#YardGain and CoverageType
-ggplot(df_4th, aes(x = pff_passCoverage, y = yardsGained)) +
-  geom_boxplot(fill = "orange", alpha = 0.7) +
-  labs(title = "Yards Gained by Defensive Coverage on 4th Down",
-       x = "Defensive Coverage Type", y = "Yards Gained") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-
-
-df_4Quarter <- subset(plays, quarter == 4 & !is.na(yardsGained))
-
-#Testing to see if 4th down is different from other downs in term of YardGain
-anova_model <- aov(yardsGained ~ factor(down), data = plays)
-summary(anova_model)
-TukeyHSD(anova_model)
-
-#Frequency table for downs
-table(plays$down)
-
-#From the anova_testing, we have not siginicant different
-#from down 1-3 but 4 down have signifcant different
-#therefore I split downs into 2 groups 1 is from 1-3 and the other one is 4th only
-plays$fourthDown <- ifelse(plays$down == 4, "4th", "1-3th")
-table(plays$fourthDown)
-
-
-
-summary(as.numeric(table(plays$gameId)))
-
-table(plays$possessionTeam)
-
-colnames(plays)
-
-colSums(is.na(plays))
-
-
-
-model_ols <- lm(
-  yardsGained ~ down + yardsToGo + quarter +
-    absoluteYardlineNumber +
-    offenseFormation + passResult + playAction +
-    pff_passCoverage + pff_manZone + rushLocationType +
-    qbSneak + qbKneel + penaltyYards,
-  data = plays
-)
-summary(model_ols)
-
-table(plays$playAction)
-
-#Clean model with few NAs data, omit those with missing data
-# Keep only relevant, mostly complete columns
-plays_clean <- subset(plays, select = c(
-  yardsGained, yardsToGo, quarter, absoluteYardlineNumber,
-  offenseFormation, playAction, pff_passCoverage, pff_manZone, gameId,fourthDown
-))
-
-# Drop rows with NAs only in this subset
-plays_clean <- na.omit(plays_clean)
-
-nrow(plays_clean)  # Should still keep thousands of rows now
-
-
-model_ols <- lm(
-  yardsGained ~fourthDown+yardsToGo + quarter +
-    absoluteYardlineNumber +
-    offenseFormation + playAction +
-    pff_passCoverage + pff_manZone,
-  data = plays_clean
-)
-
-summary(model_ols)
-
-
-table(plays_clean$pff_passCoverage)
-
-#Merge the pff_coverage smaller group, reduce coverageTypes that have rarely happen
-
-plays_clean$coverage_group <- plays_clean$pff_passCoverage
-
-plays_clean$coverage_group <- forcats::fct_collapse(
-  plays_clean$coverage_group,
-  Cover_3 = c("Cover-3", "Cover-3 Cloud Left", "Cover-3 Cloud Right", 
-              "Cover-3 Seam", "Cover-3 Double Cloud"),
-  Cover_6 = c("Cover 6-Left", "Cover-6 Right"),
-  Cover_1=c("Cover-1","Cover-1 Double"),
-  Other_Rare = c("Bracket", "Miscellaneous", "Prevent", "2-Man", "Goal Line")
-)
-
-table(plays_clean$coverage_group)
-#Re-run the model
-
-model_ols <- lm(
-  yardsGained ~fourthDown+yardsToGo + quarter +
-    absoluteYardlineNumber +
-    offenseFormation + playAction +
-    coverage_group + pff_manZone,
-  data = plays_clean
-)
-summary(model_ols)
-
-#Check for Assumption
-
-#Linearity
-
-plot(model_ols, which = 1)   # Residuals vs Fitted
-
-plot(model_ols, which = 2)  # Q–Q plot
-
-
-library(lme4)
-# Fit random intercept model
-icc_model <- lmer(yardsGained ~ fourthDown + yardsToGo + quarter +
-                    absoluteYardlineNumber + offenseFormation +
-                    playAction + coverage_group + pff_manZone +
-                    (1 | gameId),
-                  data = plays_clean)
-
-# Compute ICC
-var_comp <- as.data.frame(VarCorr(icc_model))
-icc <- var_comp$vcov[var_comp$grp == "gameId"] / sum(var_comp$vcov)
-icc
-
-
-
-
-plays_clean1 <- subset(plays, select = c(
-  yardsGained, yardsToGo, quarter, absoluteYardlineNumber,
-  offenseFormation, playAction, pff_passCoverage, pff_manZone, gameId,fourthDown,possessionTeam
-))
-
-#Those missing plays are almost certainly structural (kneels, spikes, penalties, 
-#or null plays) 
-#rather than systematically linked to performance or strategy. Therefore I drop the NA rows
-plays_clean1 <- na.omit(plays_clean1)
-
-plays_clean1$coverage_group <- plays_clean$pff_passCoverage
-
-plays_clean1$coverage_group <- forcats::fct_collapse(
-  plays_clean1$coverage_group,
-  Cover_3 = c("Cover-3", "Cover-3 Cloud Left", "Cover-3 Cloud Right", 
-              "Cover-3 Seam", "Cover-3 Double Cloud"),
-  Cover_6 = c("Cover 6-Left", "Cover-6 Right"),
-  Cover_1=c("Cover-1","Cover-1 Double"),
-  Other_Rare = c("Bracket", "Miscellaneous", "Prevent", "2-Man", "Goal Line")
-)
-# Random intercept by offensive team
-icc_offense <- lmer(
-  yardsGained ~ fourthDown + yardsToGo + quarter +
-    absoluteYardlineNumber + offenseFormation + playAction +
-    coverage_group + pff_manZone + (1 | possessionTeam),
-  data = plays_clean1
-)
-
-# Compute variance components
-var_comp_off <- as.data.frame(VarCorr(icc_offense))
-icc_possession <- var_comp_off$vcov[var_comp_off$grp == "possessionTeam"] / sum(var_comp_off$vcov)
-icc_possession
-
-
-plays_clean1 <- subset(plays, select = c(
-  yardsGained, yardsToGo, quarter, absoluteYardlineNumber,
-  offenseFormation, playAction, pff_passCoverage, pff_manZone,
-  gameId, fourthDown, possessionTeam
-))
-
-colSums(is.na(plays_clean1))
-
-
-
+library(stringr)
 
 
 
 # =========================================================
-# STEP 1: LOAD DATA AND INITIAL INSPECTION
+# STEP 0: LOAD DATA AND INITIAL INSPECTION
 # =========================================================
 
 plays <- read.csv("plays.csv")
+players <- read.csv("nfl-big-data-bowl-2025/players.csv")
 View(plays)
 
 dim(plays)        # (rows, columns)
 head(plays, 6)
 str(plays)
 
+# =========================================================
+# STEP 1: Clean and format the data
+# =========================================================
+
+plays$fourthDown <- ifelse(plays$down == 4, "4th", "1-3th")
+# Convert "mm:ss" to total seconds
+plays$gameClock_sec <- sapply(strsplit(plays$gameClock, ":"), function(x) {
+  as.numeric(x[1]) * 60 + as.numeric(x[2])
+})
+
+plays <- plays %>%
+  mutate(
+    passer_abbrev = str_extract(playDescription, "\\b[A-Z]\\.[A-Z][a-z]+")
+  )
+
+players <- players %>%
+  mutate(passer_abbrev = paste0(substr(displayName, 1, 1), ".", word(displayName, 2)))
+
+plays <- plays %>%
+  left_join(players %>% filter(position == "QB"), by = "passer_abbrev")
+
+View(plays$passer_abbrev)
+table(plays$passer_abbrev)
+
+#Filter entries that have passer apppear more than 5
+plays <- plays %>%
+  group_by(passer_abbrev) %>%       # group by the QB name/abbreviation
+  filter(n() > 5) %>%               # keep only QBs with >5 plays
+  ungroup()                         # remove grouping afterward
+
+colnames(plays)
+library(dplyr)
+
+# Keep only specific columns
+plays_small <- plays %>%
+  select(gameId, playId, playDescription, passer_abbrev,quarter, yardsGained,passResult,pff_passCoverage,
+         position.y,down,position.x,playAction)
+View(plays_small)
+table(plays_small$passer_abbrev)
+sum(is.na(plays_small$position.x))
+
+table(plays$fourthDown)
 # =========================================================
 # STEP 2: SUMMARY STATISTICS
 # =========================================================
@@ -281,9 +65,6 @@ str(plays)
 summary(plays$yardsGained)
 sd(plays$yardsGained, na.rm = TRUE)
 
-# passLength
-summary(plays$passLength)
-sd(plays$passLength, na.rm = TRUE)
 
 # =========================================================
 # STEP 3: CATEGORICAL SUMMARIES
@@ -376,8 +157,7 @@ table(plays$down)
 # From the ANOVA test:
 # No significant difference from downs 1–3, but 4th down differs
 # Therefore, split downs into two groups: 1–3 vs 4th
-plays$fourthDown <- ifelse(plays$down == 4, "4th", "1-3th")
-table(plays$fourthDown)
+
 
 # =========================================================
 # STEP 9: INITIAL MODEL WITH MANY VARIABLES
